1967f3583506efe7b2ceba77b32e21d1fe0e3d70
JBrady4 <johnny.brady04@gmail.com>
2020-01-27 13:49:02 -0500

    working demo

---
+++ Networking Demo/Assets/Scripts/CubeInput.cs
@@ -0,0 +1,111 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+using Unity.Networking.Transport;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct CubeInput : ICommandData<CubeInput>
+{
+    public uint Tick => tick;
+    public uint tick;
+    public int horizontal;
+    public int vertical;
+
+    public void Deserialize(uint tick, DataStreamReader reader, ref DataStreamReader.Context ctx)
+    {
+        this.tick = tick;
+        horizontal = reader.ReadInt(ref ctx);
+        vertical = reader.ReadInt(ref ctx);
+    }
+
+    public void Serialize(DataStreamWriter writer)
+    {
+        writer.Write(horizontal);
+        writer.Write(vertical);
+    }
+
+    public void Deserialize(uint tick, DataStreamReader reader, ref DataStreamReader.Context ctx, CubeInput baseline,
+        NetworkCompressionModel compressionModel)
+    {
+        Deserialize(tick, reader, ref ctx);
+    }
+
+    public void Serialize(DataStreamWriter writer, CubeInput baseline, NetworkCompressionModel compressionModel)
+    {
+        Serialize(writer);
+    }
+}
+
+public class NetCubeSendCommandSystem : CommandSendSystem<CubeInput>
+{
+}
+public class NetCubeReceiveCommandSystem : CommandReceiveSystem<CubeInput>
+{
+}
+
+[UpdateInGroup(typeof(ClientSimulationSystemGroup))]
+public class SampleCubeInput : ComponentSystem
+{
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<NetworkIdComponent>();
+        RequireSingletonForUpdate<EnableCubeGhostReceiveSystemComponent>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var localInput = GetSingleton<CommandTargetComponent>().targetEntity;
+        if (localInput == Entity.Null)
+        {
+            var localPlayerId = GetSingleton<NetworkIdComponent>().Value;
+            Entities.WithNone<CubeInput>().ForEach((Entity ent, ref MovableCubeComponent cube) =>
+            {
+                if (cube.PlayerId == localPlayerId)
+                {
+                    PostUpdateCommands.AddBuffer<CubeInput>(ent);
+                    PostUpdateCommands.SetComponent(GetSingletonEntity<CommandTargetComponent>(), new CommandTargetComponent { targetEntity = ent });
+                }
+            });
+            return;
+        }
+        var input = default(CubeInput);
+        input.tick = World.GetExistingSystem<ClientSimulationSystemGroup>().ServerTick;
+        if (Input.GetKey("a"))
+            input.horizontal -= 1;
+        if (Input.GetKey("d"))
+            input.horizontal += 1;
+        if (Input.GetKey("s"))
+            input.vertical -= 1;
+        if (Input.GetKey("w"))
+            input.vertical += 1;
+        var inputBuffer = EntityManager.GetBuffer<CubeInput>(localInput);
+        inputBuffer.AddCommandData(input);
+    }
+}
+
+[UpdateInGroup(typeof(GhostPredictionSystemGroup))]
+public class MoveCubeSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        var group = World.GetExistingSystem<GhostPredictionSystemGroup>();
+        var tick = group.PredictingTick;
+        var deltaTime = Time.DeltaTime;
+        Entities.ForEach((DynamicBuffer<CubeInput> inputBuffer, ref Translation trans, ref PredictedGhostComponent prediction) =>
+        {
+            if (!GhostPredictionSystemGroup.ShouldPredict(tick, prediction))
+                return;
+            CubeInput input;
+            inputBuffer.GetDataAtTick(tick, out input);
+            if (input.horizontal > 0)
+                trans.Value.x += deltaTime;
+            if (input.horizontal < 0)
+                trans.Value.x -= deltaTime;
+            if (input.vertical > 0)
+                trans.Value.z += deltaTime;
+            if (input.vertical < 0)
+                trans.Value.z -= deltaTime;
+        });
+    }
+}
+
